ef546d181e5fe2719649f0894bf38e40d10d5111
Vera <vdjakova@gmail.com>
2022-11-05 11:28:56 +0200

    Update VaistineSearchResultPage.cs

--- Page/VaistineSearchResultPage.cs
+++ Page/VaistineSearchResultPage.cs
@@ -1,42 +1,47 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
-using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
 
 namespace draft.Page
 {
     public class VaistineSearchResultPage : BasePage
     {
-        private const string orderByHighestPriceText = "Kaina: nuo didžiausios iki mažiausios";
-        private IWebElement addToCardButton => Driver.FindElement(By.CssSelector(".button-1.productAddedToCartWindowCheckout"));
-        IReadOnlyCollection<IWebElement> results => Driver.FindElements(By.CssSelector(".item-box"));
-        private SelectElement orderByDropdown => new SelectElement(Driver.FindElement(By.Id("products-orderby")));
+        private const string orderByHighestPriceText = "Aukščiausia kaina viršuje";
+        private IWebElement goToCartLink => Driver.FindElement(By.LinkText("prekių krepšelį"));
+        IReadOnlyCollection<IWebElement> results => Driver.FindElements(By.CssSelector(".category-products__cards"));
+        private SelectElement orderByDropdown => new SelectElement(Driver.FindElement(By.Id("categoryProductsSorter")));
 
         public VaistineSearchResultPage(IWebDriver webdriver) : base(webdriver) { }
         public void OrderByHighestPrice()
         {
-            GetWait().Until(driver => driver.FindElement(By.CssSelector(".page-title-holder > h1")).Displayed);
+            GetWait().Until(driver => driver.FindElement(By.CssSelector("div.category-products__description > h1")).Displayed);
             orderByDropdown.SelectByText(orderByHighestPriceText);
         }
 
         public void VerifyPrice(string price)
         {
             IWebElement firstResultElement = results.ElementAt(0);
-            string firstResultElementPrice = firstResultElement.FindElement(By.CssSelector(".price.actual-price")).Text;
+            string firstResultElementPrice = firstResultElement.FindElement(By.CssSelector(".product__price--regular")).Text;
             Assert.AreEqual(price, firstResultElementPrice, "Price is wrong");
         }
 
-        public void AddToCard()
+        public void AddToCart()
         {
             IWebElement firstResultElement = results.ElementAt(0);
             Actions action = new Actions(Driver);
             IWebElement imageElement = firstResultElement.FindElement(By.CssSelector(".picture-img"));
             action.MoveToElement(imageElement);
             action.Build().Perform();
-            firstResultElement.FindElement(By.CssSelector(".button-2.product-box-add-to-cart-button.nopAjaxCartProductListAddToCartButton")).Click();
-            addToCardButton.Click();
+            firstResultElement.FindElement(By.CssSelector(".btn.btn__primary.add-to-cart")).Click();
+        }
+
+        public void GoToCart()
+        {
+            GetWait().Until(driver => driver.FindElement(By.CssSelector(".notification__content")).Displayed);
+            goToCartLink.Click();
         }
     }
 }

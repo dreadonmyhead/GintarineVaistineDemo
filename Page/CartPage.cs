1ad49b78f139b8a44d1028af08ee162aa94102fb
Vera <vdjakova@gmail.com>
2022-11-05 11:26:32 +0200

    Update CartPage.cs

--- Page/CartPage.cs
+++ Page/CartPage.cs
@@ -6,22 +6,24 @@ namespace draft.Page
 {
     public class CartPage : BasePage
     {
-        private IWebElement totalPriceElement => Driver.FindElement(By.CssSelector(".product-subtotal"));
-        private IWebElement increaseQuantity => Driver.FindElement(By.CssSelector(".increase"));
+        private IWebElement totalPriceElement => Driver.FindElement(By.CssSelector(".cart__summary-total > .cart__summary-price"));
         public CartPage(IWebDriver webdriver) : base(webdriver) { }
 
-        public void InsertQuantity()
+        public void InsertQuantity(int quantity)
         {
-            for (int i = 1; i < 5; i++)
-            {
-                increaseQuantity.Click();
-                GetWait().Until(ExpectedConditions.InvisibilityOfElementLocated(By.CssSelector(".master-loading-overlay")));
-
-            }
+            IWebElement quantityField = Driver.FindElement(By.Name("addtocart_32657.EnteredQuantity"));
+            Actions action = new Actions(Driver);
+            action.DoubleClick(quantityField);
+            action.Build().Perform();
+            quantityField.SendKeys(quantity.ToString());
+            action.KeyDown(Keys.Enter);
+            action.KeyUp(Keys.Enter);
+            action.Build().Perform();
         }
 
         public void VerifyIfICanBuy(int moneyToSpent)
         {
+            GetWait().Until(SeleniumExtras.WaitHelpers.ExpectedConditions.InvisibilityOfElementLocated(By.CssSelector(".notification notification--success")));
             double totalPrice = double.Parse(totalPriceElement.Text.Replace("€", ""));
             Assert.IsTrue(moneyToSpent > totalPrice, $"Cannot by 5 mollers with 100€, total price is {totalPrice}");
         }
